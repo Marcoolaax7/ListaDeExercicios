22c3af950660ecbbd1deb4ff2691a5cccc1dea4b
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 18:06:23 -0300

    Exercicio12

--- Exercicio04/obj/Debug/net10.0/Exercicio04.AssemblyInfo.cs
+++ Exercicio04/obj/Debug/net10.0/Exercicio04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8556e870606627d053f6903828012157de58fea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700fb4596434a0f57763c603cc9789cf6e953ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercicio12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%. Após o aumento, desconte 8% de
+//impostos. Imprima o salário inicial, o salário com o aumento e o salário final.
+
+
+Console.Write("Digite o salario inicial: ");
+decimal salarioInicial = Convert.ToDecimal(Console.ReadLine());
+
+decimal aumentoSalario = salarioInicial + (salarioInicial * 0.15m);
+
+decimal salarioFinal = aumentoSalario - (aumentoSalario * 0.08m);
+
+Console.WriteLine($"O salario inicial é de R${salarioInicial},o aumento do salario em 15% ficou em R${aumentoSalario} e o salario final ja tirando o 8% de imposto é de R${salarioFinal}.");
+
+Console.ReadLine();

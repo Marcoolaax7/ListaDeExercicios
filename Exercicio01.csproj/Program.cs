3346604c8b265dd096f43ee7227df842f8f564cd
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 14:08:21 -0300

    Adicionado Exercicio 01

---
+++ Exercicio01.csproj/Program.cs
@@ -0,0 +1,18 @@
+﻿// Crie um programa para calcular o volume de uma caixa retangular
+//multiplique o comprimento  largura \(\times \) altura (ou profundidade)
+
+Console.WriteLine("Digite o comprimento da caixa ");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a largura da caixa ");
+decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a altura da caixa ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal volume = comprimento * largura * altura;
+
+Console.WriteLine($"O volume da caixa retangular é: {volume} cm3");
+
+
+Console.ReadLine();

8556e870606627d053f6903828012157de58fea1
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 17:27:18 -0300

    Exercicio10

--- Exercicio08/obj/Debug/net10.0/Exercicio08.AssemblyInfo.cs
+++ Exercicio08/obj/Debug/net10.0/Exercicio08.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+108660aae758a618d4b8388e960c4c0593a9d04f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca26820d7a78299460833fcf2740236eb4387f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercicio10/Program.cs
@@ -0,0 +1,22 @@
+﻿// A padaria Hotpão vende uma certa quantidade de pães franceses e uma quantidade de broas a cada dia. Cada
+//pãozinho custa R$ 0,12 e a broa custa R$ 1,50. Ao final do dia, o dono quer saber quanto arrecadou com a venda
+//dos pães e broas (juntos), e quanto deve guardar numa conta de poupança (10% do total arrecadado). Você foi
+//contratado para fazer os cálculos para o dono. Com base nestes fatos, faça um algoritmo para ler as quantidades
+//de pães e de broas, e depois calcular os dados solicitados.
+
+Console.Clear();
+
+Console.Write("Quantidade de paes vendidas: ");
+double quantidadePaes = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Quantidade de broas vendidas: ");
+double quantidadeBroas = Convert.ToDouble(Console.ReadLine());
+
+double precoPao = 0.12;
+double precoBroa = 1.50;
+
+double precoJunto = precoPao * quantidadePaes + precoBroa * quantidadeBroas;
+
+double valorParaPoupanca = precoJunto * 0.10;
+
+Console.Write($"Quantidade total ventida foi de: R${precoJunto} e o valor para guardar na poupança é de R${valorParaPoupanca.ToString("f2")} reais");

700fb4596434a0f57763c603cc9789cf6e953ecb
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 17:35:45 -0300

    Exercicio11

--- Exercicio03/obj/Debug/net10.0/Exercicio03.AssemblyInfo.cs
+++ Exercicio03/obj/Debug/net10.0/Exercicio03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca26820d7a78299460833fcf2740236eb4387f9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8556e870606627d053f6903828012157de58fea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercicio11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida ela possui.
+//Considere sempre anos completos, e que um ano possui 365 dias. Ex: uma pessoa com 19 anos possui 6935 dias
+//de vida; veja um exemplo de saída: MARIA, VOCÊ JÁ VIVEU 6935 DIAS.
+
+Console.WriteLine("Digite o seu nome: ");
+string? nome = Console.ReadLine();
+
+Console.WriteLine("Digite a sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+decimal diasDeVida = idade * 365m;
+
+Console.WriteLine($"{nome} voce ja viveu {diasDeVida} dias!");

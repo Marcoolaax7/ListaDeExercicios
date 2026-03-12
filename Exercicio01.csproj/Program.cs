a2b16057fdcce8933ce2cc5daf44d0309ab1ae43
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 14:11:04 -0300

    Exercicio 1

--- Exercicio01.csproj/Program.cs
+++ Exercicio01.csproj/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Crie um programa para calcular o volume de uma caixa retangular
 //multiplique o comprimento  largura \(\times \) altura (ou profundidade)
 
-Console.WriteLine("Digite o comprimento da caixa ");
+Console.WriteLine("Digite o comprimento da caixa: ");
 decimal comprimento = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Digite a largura da caixa ");
+Console.WriteLine("Digite a largura da caixa: ");
 decimal largura = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Digite a altura da caixa ");
+Console.WriteLine("Digite a altura da caixa: ");
 decimal altura = Convert.ToDecimal(Console.ReadLine());
 
 decimal volume = comprimento * largura * altura;
--- Exercicio01.csproj/obj/Debug/net10.0/Exercicio01.csproj.AssemblyInfo.cs
+++ Exercicio01.csproj/obj/Debug/net10.0/Exercicio01.csproj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio01.csproj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3346604c8b265dd096f43ee7227df842f8f564cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio01.csproj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio01.csproj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

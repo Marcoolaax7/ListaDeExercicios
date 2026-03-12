48809f0b9ad9c57e754d707071d3071fd39af042
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 14:45:55 -0300

    Exercicios

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Crie um programa para calcular o volume de um Cilindro
+// 
+
+Console.Clear();
+
+Console.WriteLine("Digite o raio do cilindro: ");
+double raio = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a altura do cilindro: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+ 
+
+double areaBase = Math.PI * Math.Pow(raio, 2);
+
+double volume = areaBase * altura;
+
+Console.WriteLine($"O volume do cilindro é: {volume} cm3");
+
+
+Console.ReadLine();
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma viagem.
+// O programa deve solicitar ao usuário:
+//● A quilometragem inicial do veículo no início da viagem.
+//● A quilometragem final ao término da viagem.
+//● A quantidade de combustível consumida durante a viagem (em litros).
+
+
+Console.WriteLine("Digite a quilometragem inicial do veiculo: ");
+decimal kmInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a quilometragem final do veiculo: ");
+decimal kmFinal = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade de combustivel consumida (litros): ");
+decimal combustivelConsumido = Convert.ToDecimal(Console.ReadLine());
+
+decimal distanciaPercorrida = kmFinal = kmInicial;
+
+decimal consumoPorKm = distanciaPercorrida / combustivelConsumido;
+
+Console.WriteLine($"O consumo de combustivel durante o percusso foi de ~: {consumoPorKm} km/");
+
+Console.ReadLine();
+
+

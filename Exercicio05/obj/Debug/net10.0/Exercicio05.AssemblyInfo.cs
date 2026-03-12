ab5e6f4ac6d1ad9274da194c364a33c93ad3fb88
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 18:17:29 -0300

    Exercicio13

--- Exercicio05/obj/Debug/net10.0/Exercicio05.AssemblyInfo.cs
+++ Exercicio05/obj/Debug/net10.0/Exercicio05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700fb4596434a0f57763c603cc9789cf6e953ecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c3af950660ecbbd1deb4ff2691a5cccc1dea4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercicio13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
+Console.WriteLine("Digite o valor de A: ");
+decimal vA = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B: ");
+decimal vB = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de C: ");
+decimal vC = Convert.ToDecimal(Console.ReadLine());
+
+if (vA + vB < vC)
+{
+    Console.WriteLine("A soma de a + b é menor que c.");
+}
+else
+{
+    Console.WriteLine("A soma de a + b nao é menor que c!");
+}

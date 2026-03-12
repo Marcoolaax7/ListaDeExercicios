e27388c86d1b2f0690a42af474dfd904413bf7ad
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-12 18:32:58 -0300

    Exercicio14

--- Exercicio08/obj/Debug/net10.0/Exercicio08.AssemblyInfo.cs
+++ Exercicio08/obj/Debug/net10.0/Exercicio08.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c3af950660ecbbd1deb4ff2691a5cccc1dea4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab5e6f4ac6d1ad9274da194c364a33c93ad3fb88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercicio14/Program.cs
@@ -0,0 +1,42 @@
+﻿//Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
+
+
+
+
+Console.Clear();
+
+Console.WriteLine("Digite o primeiro valor: ");
+decimal v1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo valor: ");
+decimal v2 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro valor: ");
+decimal v3 = Convert.ToDecimal(Console.ReadLine());
+
+if (v1 > v2 && v1 > v3)
+{
+    if (v2 > v3)
+        Console.WriteLine($"Ordem decescente: {v1},{v2},{v3}.");
+    else
+        Console.WriteLine($"Ordem decescente: {v1},{v3},{v2}.");
+
+}
+else if (v2 > v1 && v2 > v3)
+{
+    if (v1 > v3)
+        Console.WriteLine($"Ordem decescente: {v2},{v1},{v3}.");
+    else
+        Console.WriteLine($"Ordem decescente: {v2},{v3},{v1}.");
+
+}
+else
+{
+    if (v1 > v2)
+        Console.WriteLine($"Ordem decescente: {v3},{v1},{v2}.");
+    else
+        Console.WriteLine($"Ordem decescente: {v3},{v2},{v1}.");
+
+}
+
+

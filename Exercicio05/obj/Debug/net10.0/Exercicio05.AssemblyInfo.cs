1b671a33971a752bf899141c937bbef23ace83f1
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-13 14:15:56 -0300

    Exercicio15

--- Exercicio05/obj/Debug/net10.0/Exercicio05.AssemblyInfo.cs
+++ Exercicio05/obj/Debug/net10.0/Exercicio05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab5e6f4ac6d1ad9274da194c364a33c93ad3fb88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27388c86d1b2f0690a42af474dfd904413bf7ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercicio15/Program.cs
@@ -0,0 +1,35 @@
+﻿// O IMC – Índice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre
+//a condição de peso de uma pessoa adulta. A fórmula é IMC = peso / (altura) 2. Elabore um algoritmo que leia o
+//peso e a altura de um adulto e mostre sua condição de acordo com a listagem abaixo:
+//● IMC em adultos Condição
+//● Abaixo de 18,5
+//● Abaixo do peso
+//● Entre 18,5 e 25 Peso normal
+//● Entre 25 e 30
+//● Acima do peso
+//● Acima de 30 obeso
+
+Console.Write("Digite seu peso (kg): ");
+decimal peso = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite sua altura (m): ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal imc = peso / (altura * altura); 
+
+if (imc < 18.5m)
+{
+    Console.WriteLine("Voce esta a baixo do peso: ");
+}
+else if (imc >= 18.5m && imc < 25)
+{
+    Console.WriteLine("Voce esta com o peso normal!");
+}
+else if (imc >= 25 && imc < 30)
+{
+    Console.WriteLine("Voce esta acima do peso!");
+}
+else
+{
+    Console.WriteLine("Voce esta obeso");
+}
